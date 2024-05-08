07374a4acf82ff7da1f398e5b13b6b720611204f
Mordechai Dror <vorant94@pm.me>
2024-05-08 16:03:25 +0300

    init commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+  singleQuote: true,
+  bracketSameLine: true,
+  singleAttributePerLine: true,
+  plugins: [
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-css-order',
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { type FC } from 'react';
+
+export const App: FC = function () {
+  return <>Hello World!</>;
+};
