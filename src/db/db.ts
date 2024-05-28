d92755a2da95e1e3a0b382467f329cf6850ab9e0
Mordechai Dror <vorant94@pm.me>
2024-05-28 09:52:26 +0300

    get dir of global.d.ts since it assumed there is only one dexie instance per app

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,18 @@
-import Dexie from 'dexie';
+import type {
+  InsertSubscriptionModel,
+  SubscriptionModel,
+} from '@/subscriptions/models/subscription.model.ts';
+import Dexie, { type Table } from 'dexie';
 
-export const db = new Dexie('subs-savvy');
+export const db = new Dexie('subs-savvy') as Dexie & {
+  subscriptions: Table<RawSubscriptionModel>;
+};
 
 db.version(1).stores({
   subscriptions: '++id',
 });
+
+// This is needed because Dexie doesn't distinguish between mutation and query models
+// hence requiring to pass id while creating row even though it is auto-generating under the hood
+type RawSubscriptionModel = InsertSubscriptionModel &
+  Partial<Pick<SubscriptionModel, 'id'>>;
