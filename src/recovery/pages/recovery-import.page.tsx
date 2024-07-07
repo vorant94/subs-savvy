a5ede18260a3306cc08db5c6dc401cbcee47ee4d
Mordechai Dror <vorant94@pm.me>
2024-07-07 20:07:00 +0300

    replace mantine dropzone with react dropzone

--- src/recovery/pages/recovery-import.page.tsx
+++ src/recovery/pages/recovery-import.page.tsx
@@ -4,49 +4,26 @@ import {
   type InsertSubscriptionModel,
 } from '@/subscriptions/models/subscription.model.ts';
 import { cn } from '@/ui/utils/cn.ts';
-import { faFileCode } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Button, Text } from '@mantine/core';
-import { Dropzone } from '@mantine/dropzone';
-import { memo, useCallback, useEffect, useState } from 'react';
-import type { FileWithPath } from 'react-dropzone-esm';
-import { recoverySchema } from '../models/recovery.model.ts';
+import { Button } from '@mantine/core';
+import { memo, useCallback, useState } from 'react';
+import {
+  RecoveryImportDropZone,
+  type RecoveryImportDropZoneProps,
+} from '../components/recovery-import-drop-zone.tsx';
 
 export const RecoveryImportPage = memo(() => {
   const [subscriptions, setSubscriptions] = useState<
     Array<InsertSubscriptionModel>
   >([]);
 
-  const readExportFile = useCallback(
-    ([file]: Array<FileWithPath>) => file && reader.readAsText(file),
-    [],
-  );
-  useEffect(() => {
-    reader.addEventListener('load', processExportFile);
-
-    return () => {
-      reader.removeEventListener('load', processExportFile);
-    };
-  });
-  const processExportFile = ({ currentTarget }: ProgressEvent<FileReader>) => {
-    if (!currentTarget) {
-      throw new Error(`currentTarget is missing`);
-    }
-
-    const { result } = currentTarget as FileReader;
-    if (typeof result !== 'string') {
-      throw new Error(`type of result should be string`);
-    }
-
-    // TODO validate dbVersion
-    const { subscriptions } = recoverySchema.parse(JSON.parse(result));
-
-    setSubscriptions(
-      subscriptions.map((subscription) =>
-        insertSubscriptionSchema.parse({ ...subscription, tags: [] }),
-      ),
-    );
-  };
+  const setSubscriptionFromRecovery: RecoveryImportDropZoneProps['onRecoveryParsed'] =
+    useCallback(({ subscriptions }) => {
+      setSubscriptions(
+        subscriptions.map((subscription) =>
+          insertSubscriptionSchema.parse({ ...subscription, tags: [] }),
+        ),
+      );
+    }, []);
 
   const [formId, setFormId] = useState('');
   const updateFormId: (ref: HTMLFormElement | null) => void = useCallback(
@@ -60,38 +37,7 @@ export const RecoveryImportPage = memo(() => {
 
   return (
     <div className={cn(`flex flex-col gap-4`)}>
-      {/* Playwright won't work without useFsAccessApi see https://github.com/microsoft/playwright/issues/8850 */}
-      <Dropzone
-        useFsAccessApi={false}
-        component={'button'}
-        aria-label={`click or drag & drop to upload file`}
-        onDrop={readExportFile}
-        multiple={false}
-        accept={['application/json']}>
-        <div
-          className={cn(
-            `pointer-events-none flex min-h-40 items-center justify-center gap-4`,
-          )}>
-          <Dropzone.Idle>
-            <FontAwesomeIcon
-              style={{
-                color: 'var(--mantine-color-dimmed)',
-              }}
-              size="4x"
-              icon={faFileCode}
-            />
-          </Dropzone.Idle>
-
-          <div className={cn(`flex flex-col gap-1`)}>
-            <Text size="xl">Drag file here or click to select it</Text>
-            <Text
-              size="sm"
-              c="dimmed">
-              Attach one file of JSON format
-            </Text>
-          </div>
-        </div>
-      </Dropzone>
+      <RecoveryImportDropZone onRecoveryParsed={setSubscriptionFromRecovery} />
 
       {subscriptions.length ? (
         <>
@@ -115,5 +61,3 @@ export const RecoveryImportPage = memo(() => {
     </div>
   );
 });
-
-const reader = new FileReader();
