9d08c6c2e8f4eeb87da5e7c8920fc53181b2bf88
Mordechai Dror <vorant94@pm.me>
2024-07-07 20:40:11 +0300

    add more test

---
+++ src/subscriptions/utils/is-subscription-expired.spec.ts
@@ -0,0 +1,44 @@
+import dayjs from 'dayjs';
+import { describe, expect, it } from 'vitest';
+import { monthlySubscription } from '../models/subscription.mock.ts';
+import type { SubscriptionModel } from '../models/subscription.model.ts';
+import { isSubscriptionExpired } from '../utils/is-subscription-expired.ts';
+
+describe('isSubscriptionExpired', () => {
+  it('no endedAt', () => {
+    const subscription = { ...monthlySubscription } satisfies SubscriptionModel;
+    const compareTo = dayjs(subscription.startedAt).add(1, 'day').toDate();
+
+    expect(isSubscriptionExpired(subscription, compareTo)).toBeFalsy();
+  });
+
+  it('endedAt < compareTo', () => {
+    const subscription = {
+      ...monthlySubscription,
+      endedAt: dayjs(monthlySubscription.startedAt).add(1, 'month').toDate(),
+    } satisfies SubscriptionModel;
+    const compareTo = dayjs(subscription.endedAt).add(1, 'day').toDate();
+
+    expect(isSubscriptionExpired(subscription, compareTo)).toBeTruthy();
+  });
+
+  it('endedAt = compareTo', () => {
+    const subscription = {
+      ...monthlySubscription,
+      endedAt: dayjs(monthlySubscription.startedAt).add(1, 'month').toDate(),
+    } satisfies SubscriptionModel;
+    const compareTo = dayjs(subscription.endedAt).startOf('day').toDate();
+
+    expect(isSubscriptionExpired(subscription, compareTo)).toBeFalsy();
+  });
+
+  it('compareTo < endedAt', () => {
+    const subscription = {
+      ...monthlySubscription,
+      endedAt: dayjs(monthlySubscription.startedAt).add(1, 'month').toDate(),
+    } satisfies SubscriptionModel;
+    const compareTo = dayjs(subscription.endedAt).subtract(1, 'day').toDate();
+
+    expect(isSubscriptionExpired(subscription, compareTo)).toBeFalsy();
+  });
+});
--- src/subscriptions/utils/is-subscription-expired.ts
+++ src/subscriptions/utils/is-subscription-expired.ts
@@ -3,8 +3,15 @@ import type { SubscriptionModel } from '../models/subscription.model.ts';
 
 export function isSubscriptionExpired(
   subscription: SubscriptionModel,
+  compareTo = new Date(),
 ): boolean {
-  return (
-    !!subscription.endedAt && dayjs(subscription.endedAt).isBefore(new Date())
-  );
+  if (!subscription.endedAt) {
+    return false;
+  }
+
+  if (dayjs(subscription.endedAt).isSame(compareTo, 'day')) {
+    return false;
+  }
+
+  return dayjs(subscription.endedAt).isBefore(compareTo);
 }
