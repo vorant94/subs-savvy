7b2648178671c3f8f27dd134e8881fcdfc1cb971
Mordechai Dror <vorant94@pm.me>
2024-07-08 13:30:11 +0300

    make coverage more precise

--- vite.config.mjs
+++ vite.config.mjs
@@ -22,13 +22,19 @@ export default defineConfig({
       exclude: [
         '**/types/*',
         '**/globals/*',
-        '**/models/*',
+        '**/models/*.model.ts',
+        '**/models/*.model.tsx',
+        '**/models/*.mock.ts',
+        '**/models/*.mock.tsx',
         '**/pages/*.pom.ts',
         '**/pages/*.pom.tsx',
         '**/components/*.com.ts',
         '**/components/*.com.tsx',
         'db/utils/populate-db.ts', // utility function for tests only
         'db/utils/clean-up-db.ts', // utility function for tests only
+        'ui/hooks/use-breakpoint.ts', // a simple wrapper with mapped arguments
+        'ui/utils/cn.ts', // a simple wrapper to unify two external libs
+        'main.tsx', // main setup file
       ],
     },
   },
