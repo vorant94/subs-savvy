3102a43bf8b7c6513c238cf5c83c47d9c29e5534
Mordechai Dror <vorant94@pm.me>
2024-07-17 16:30:18 +0300

    mock and spy on a child component

--- src/subscriptions/components/subscription-list.spec.tsx
+++ src/subscriptions/components/subscription-list.spec.tsx
@@ -8,6 +8,7 @@ import {
   describe,
   expect,
   it,
+  type MockInstance,
   vi,
 } from 'vitest';
 import { useSubscriptions } from '../hooks/use-subscriptions';
@@ -16,19 +17,28 @@ import {
   monthlySubscription,
   yearlySubscription,
 } from '../models/subscription.mock.ts';
+import type { SubscriptionModel } from '../models/subscription.model.ts';
 import { SubscriptionList } from './subscription-list';
+import { SubscriptionListItem } from './subscription-list-item.tsx';
 
 vi.mock(import('../hooks/use-subscriptions.tsx'));
+vi.mock(import('./subscription-list-item.tsx'));
 
 describe('SubscriptionList', () => {
   let screen: RenderResult;
+  let listItemSpy: MockInstance;
+
+  beforeAll(() => {
+    vi.mocked(SubscriptionListItem.type).mockReturnValue(<></>);
+    listItemSpy = vi.spyOn(SubscriptionListItem, 'type');
+  });
 
   beforeEach(() => {
     screen = render(<SubscriptionList />, { wrapper });
   });
 
   afterEach(() => {
-    vi.restoreAllMocks();
+    vi.clearAllMocks();
   });
 
   describe('with data', () => {
@@ -39,9 +49,20 @@ describe('SubscriptionList', () => {
       });
     });
 
-    it.todo('should render list items');
+    it('should render list items instead of no subscription placeholder', async () => {
+      const subscriptions = [
+        monthlySubscription,
+        yearlySubscription,
+      ] satisfies ReadonlyArray<SubscriptionModel>;
 
-    it('should hide no subscription placeholder', async () => {
+      await Promise.all(
+        subscriptions.map((subscription) =>
+          waitFor(() =>
+            expect(listItemSpy).toHaveBeenCalledWith({ subscription }, {}),
+          ),
+        ),
+      );
+
       await waitFor(() =>
         expect(screen.queryByText('No Subscriptions')).not.toBeInTheDocument(),
       );
@@ -53,7 +74,9 @@ describe('SubscriptionList', () => {
       vi.mocked(useSubscriptions).mockReturnValue({ ...useSubscriptionsMock });
     });
 
-    it('should show no subscription placeholder', async () => {
+    it('should show no subscription placeholder instead of list items', async () => {
+      expect(listItemSpy).not.toHaveBeenCalled();
+
       await waitFor(() =>
         expect(screen.queryByText('No Subscriptions')).toBeVisible(),
       );
