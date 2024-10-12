6d77f1888606eff12493c7d208091a62a6d6e131
Mordechai Dror <vorant94@pm.me>
2024-10-12 15:54:40 +0300

    remove irrelevant copy, use wrangler from package.json

--- src/pages/recovery/ui/recovery.page.tsx
+++ src/pages/recovery/ui/recovery.page.tsx
@@ -1,4 +1,4 @@
-import { Card, Tabs, Text } from "@mantine/core";
+import { Card, Tabs } from "@mantine/core";
 import { IconDownload, IconUpload } from "@tabler/icons-react";
 import { memo, useCallback, useMemo } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
@@ -25,47 +25,36 @@ export const RecoveryPage = memo(() => {
 
 	return (
 		<DefaultLayout header={<DefaultLayoutHeader />}>
-			<div className={cn("flex flex-col gap-4")}>
-				<Card
-					shadow="xs"
-					padding="xs"
-					radius="md"
-					withBorder
-				>
-					<div className={cn("flex flex-col gap-2")}>
-						<Tabs
-							value={activeTab}
-							onChange={navigateToTab}
-						>
-							<Tabs.List>
-								<Tabs.Tab
-									value={recoveryRoute.import}
-									leftSection={<Icon icon={IconUpload} />}
-								>
-									Import
-								</Tabs.Tab>
-								<Tabs.Tab
-									value={recoveryRoute.export}
-									leftSection={<Icon icon={IconDownload} />}
-								>
-									Export
-								</Tabs.Tab>
-							</Tabs.List>
-						</Tabs>
+			<Card
+				shadow="xs"
+				padding="xs"
+				radius="md"
+				withBorder
+			>
+				<div className={cn("flex flex-col gap-2")}>
+					<Tabs
+						value={activeTab}
+						onChange={navigateToTab}
+					>
+						<Tabs.List>
+							<Tabs.Tab
+								value={recoveryRoute.import}
+								leftSection={<Icon icon={IconUpload} />}
+							>
+								Import
+							</Tabs.Tab>
+							<Tabs.Tab
+								value={recoveryRoute.export}
+								leftSection={<Icon icon={IconDownload} />}
+							>
+								Export
+							</Tabs.Tab>
+						</Tabs.List>
+					</Tabs>
 
-						<Outlet />
-					</div>
-				</Card>
-
-				<Text
-					className={cn("max-w-80 self-center text-balance text-center")}
-					size="sm"
-					c="dimmed"
-				>
-					Currently only subscriptions are supported during import/export. All
-					the categories and their subscription linkage are ignored.
-				</Text>
-			</div>
+					<Outlet />
+				</div>
+			</Card>
 		</DefaultLayout>
 	);
 });
