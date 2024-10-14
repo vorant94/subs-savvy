27d3b5b92680c186052a1f7e5de707fd83970c6b
Mordechai Dror <vorant94@pm.me>
2024-10-14 12:01:12 +0300

    enforce docs or non-docs on workflow level

--- scripts/or-docs-or-non-docs.js
+++ scripts/or-docs-or-non-docs.js
@@ -1,6 +1,18 @@
+import { parseArgs } from "node:util";
+import { z } from "zod";
 import { exec } from "./shared/promisified.js";
 
-const diffRaw = await exec("git diff --name-only HEAD");
+const argsRaw = parseArgs({
+	options: {
+		target: { type: "string", short: "t" },
+	},
+});
+
+const args = z
+	.object({ target: z.string().nullable().default("HEAD") })
+	.parse(argsRaw.values);
+
+const diffRaw = await exec(`git diff --name-only ${args.target}`);
 const diffFiles = diffRaw.stdout.split("\n").filter(Boolean);
 
 const [docs, nonDocs] = diffFiles.reduce(
