6c1db5e2b476d240ac0b06fb1a5f97bfae09f8ad
Mordechai Dror <vorant94@pm.me>
2024-10-14 11:39:53 +0300

    make is docs only to be node script

---
+++ scripts/is-non-docs-change.js
@@ -0,0 +1,11 @@
+import process from "node:process";
+import { exec } from "./shared/promisified.js";
+
+const diffRaw = await exec("git diff --name-only HEAD^");
+const diffFiles = diffRaw.stdout.split("\n").filter(Boolean);
+
+const docs = diffFiles.filter((f) => f.startsWith("docs/"));
+
+if (docs.length) {
+	process.exit(1);
+}
